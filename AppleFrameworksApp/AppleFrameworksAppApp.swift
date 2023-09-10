9a438b7cc2a065bad6943b328c614bc0c2f60f4a
Kitso Gidion Namane <kitsogideonnamane@gmail.com>
2023-09-10 12:07:57 +0200

    Initial Commit

---
+++ AppleFrameworksApp/AppleFrameworksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksAppApp.swift
+//  AppleFrameworksApp
+//
+//  Created by Kitso Namane on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
